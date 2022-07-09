9b73f1dd77a95a3eeba3d940a4d532cf80ad7fda
Tarun Choudhary <79400336+Tarun830744@users.noreply.github.com>
2022-07-10 00:24:57 +0530

    Delete Assignemnt 7.sql

--- Assignemnt 7.sql
+++
@@ -1,5 +0,0 @@
-# Q1 - Write an SQL query to show only odd rows from a table.
-SELECT * FROM Worker WHERE MOD(worker_id,2) != 0;
-
-# Q2 - Write an SQL query to clone a new table from another table.
-SELECT * INTO Worker_clone FROM Worker;
